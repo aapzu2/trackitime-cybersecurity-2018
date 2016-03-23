aaeba56118bfa7d66c6901c0fc09bac5ab496815
Aapeli Haanpuu <aapzu@iki.fi>
2016-03-23 13:01:56 +0200

    Update db-client.js

--- app/db-client.js
+++ app/db-client.js
@@ -3,8 +3,8 @@ function DBClient() {
     var pg = require('pg')
 
     pg.defaults.ssl = true
-
-    var host = process.env.DATABASE_URL || 'postgres://ixvgkdrmthzqod:ty0AGdMZfuzTA95XdOGvAsfLK7@ec2-54-243-149-147.compute-1.amazonaws.com:5432/d1edm9enke35bt'
+ 
+    var host = process.env.DATABASE_URL
 
     var client = new pg.Client(host)
     client.connect(function(err) {
