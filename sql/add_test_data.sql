c04b93cd1914ff5c5505503ad1cfc3d974d22656
Aapeli Haanpuu <aapzu@iki.fi>
2016-04-11 21:21:26 +0300

    Update add_test_data.sql

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,30 +1,30 @@
-INSERT INTO "User" ("name", "username", "password", "isAdmin")
-VALUES ('Admin Tester', 'admin', '$2a$04$w5ys/4PwdFlIhcuTZuE07OTtvbkmHuwaRgQJCFaPWmDkjagBWpQoK', '1') 
+INSERT INTO "User" ("id", "name", "username", "password", "isAdmin")
+VALUES ('0', 'Admin Tester', 'admin', '$2a$04$w5ys/4PwdFlIhcuTZuE07OTtvbkmHuwaRgQJCFaPWmDkjagBWpQoK', '1') 
 RETURNING "id", "name", "username", "password", "isAdmin";
 
-INSERT INTO "User" ("name", "username", "password", "isAdmin")
-VALUES ('Basic Tester', 'tester', '$2a$04$cvW4XJmvUWt1eO3v0lX.QOLcmZv.I2Iktkrq8d.U2of7YTVSVRbay', '0') 
+INSERT INTO "User" ("id", "name", "username", "password", "isAdmin")
+VALUES ('1', 'Basic Tester', 'tester', '$2a$04$cvW4XJmvUWt1eO3v0lX.QOLcmZv.I2Iktkrq8d.U2of7YTVSVRbay', '0') 
 RETURNING "id", "name", "username", "password", "isAdmin";
 
-INSERT INTO "Project"("name", "started", "description") 
-VALUES('Muutto', '2016-04-01', 'Muutetaan uuteen kotiin') 
+INSERT INTO "Project"("id", "name", "started", "description") 
+VALUES('0', 'Muutto', '2016-04-01', 'Muutetaan uuteen kotiin') 
 RETURNING "id", "name", "started", "description";
 
-INSERT INTO "Project"("name", "started", "description") 
-VALUES('HIMYM', '2016-03-24', 'Pakko katsoa') 
+INSERT INTO "Project"("id", "name", "started", "description") 
+VALUES('1', 'HIMYM', '2016-03-24', 'Pakko katsoa') 
 RETURNING "id", "name", "started", "description";
 
-INSERT INTO "UserProject"("user", "project") 
+INSERT INTO "UserProject"("user", "project", "isAdmin") 
 VALUES('0', '0', '1')
-RETURNING "user", "project";
+RETURNING "user", "project", "isAdmin";
 
-INSERT INTO "UserProject"("user", "project") 
+INSERT INTO "UserProject"("user", "project", "isAdmin") 
 VALUES('1', '0', '0')
-RETURNING "user", "project";
+RETURNING "user", "project", "isAdmin";
 
-INSERT INTO "UserProject"("user", "project") 
+INSERT INTO "UserProject"("user", "project", "isAdmin") 
 VALUES('0', '1', '1')
-RETURNING "user", "project";
+RETURNING "user", "project", "isAdmin";
 
 INSERT INTO "TimeInstance"("description", "from", "to", "project", "user") 
 VALUES('Watched the first two seasons', '2016-03-01 12:00', '2016-03-01 14:00', '0', '0') 
@@ -33,4 +33,3 @@ RETURNING "id", "description", "from", "to", "project", "user";
 INSERT INTO "TimeInstance"("description", "from", "to", "project", "user") 
 VALUES('Kamat sisään', '2016-04-02 10:00', '2016-04-02 13:00', '1', '0') 
 RETURNING "id", "description", "from", "to", "project", "user";
-
